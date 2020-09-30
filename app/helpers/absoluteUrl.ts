dd17b18869f1caa4edda3bd9bb6895271967b080
Dominik <dominik.tomcik23@gmail.com>
2020-09-30 12:25:32 +0200

    vercel fix

--- app/helpers/absoluteUrl.ts
+++ app/helpers/absoluteUrl.ts
@@ -1,7 +1,6 @@
 export function absoluteUrl(localhost: string): string {
   const isDev = process.env.NODE_ENV === `development`;
-  if (isDev) {
-    return `http://${localhost}`;
-  }
-  return window.location.origin;
+  if (isDev) return `http://${localhost}`;
+
+  return `https://pars-shop-server.now.sh`;
 }
